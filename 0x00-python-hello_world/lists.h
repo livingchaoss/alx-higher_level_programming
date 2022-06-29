fb7e8b0005a376a22f096128e90783db690fd9c4
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-06-29 08:07:15 -0700

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,21 +5,21 @@
 
 /**
  * struct listint_s - singly linked list
- * @n : integer
- * @next : points ti the next node
- * 
+ * @n: integer
+ * @next: points to the next node
+ *
  * Description: singly linked list node structure
- * for alx project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listints_s *next;
-}listint_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /*LIST_H*/
+#endif /* LISTS_H */
