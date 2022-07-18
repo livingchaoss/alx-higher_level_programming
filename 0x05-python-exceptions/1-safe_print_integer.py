069dc1f53ecdcb061adbc746823dea6aca6723e0
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-07-18 03:59:22 -0700

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def safe_Print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except:
-        return False
+def safe_print_list_integers(my_list=[], x=0):
+    a = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list=[i]), end="")
+            a += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return a
