1f0fbf85b7962894434f80562372bb5225a4f65c
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-07-25 07:59:31 -0700

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,7 +9,6 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
-        
         Args:
             width: width of the rectangle
             height: height of the rectangle
@@ -25,7 +24,6 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets the width of a Rectangle instance
-
         Args:
             value: value of the width, must be a positive integer
         """
@@ -43,7 +41,6 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height of a Rectangle instance
-
         Args:
             value: value of the height, must be a positive integer
         """
@@ -55,7 +52,6 @@ class Rectangle:
 
     def area(self):
         """Calculates the area of a Rectangle instance
-
         Returns:
             Area of the the rectangle, given by height * width
         """
@@ -63,7 +59,6 @@ class Rectangle:
 
     def perimeter(self):
         """Calculates the perimeter of a Rectangle instance
-
         Returns:
             Perimeter of the rectangle, given by 2 * (height + width)
         """
