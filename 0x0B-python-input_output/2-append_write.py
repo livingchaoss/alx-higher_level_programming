acf0cccc32d161d52bd497c2965ecf3f424de471
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-08-02 05:33:30 -0700

    Create 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module 2-append_write.
+Appends a String at the end of a text file.
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends text to filename
+
+    Args:
+        filename: name of the file
+        text: text to append
+    
+    Returns: the number of characters added
+    """
+
+with open(filename, 'a+') as f:
+    return f.write(text)
+    
