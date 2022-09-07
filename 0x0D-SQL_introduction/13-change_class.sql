a67f2fab6647f990e10d88490abb13cbdabb2ec6
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-09-07 06:18:41 -0700

    Create 13-change_class.sql

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all records with a score <= 5 in the table of the database hbtn_0c_0 in your MySQL server
+-- removes records with fitting a condition
+DELETE FROM second_table WHERE `score` <= 5;
