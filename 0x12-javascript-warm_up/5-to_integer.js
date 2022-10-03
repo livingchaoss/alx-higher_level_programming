2f69da116824759045b40fdcade7601d1d55dc4a
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-10-03 13:54:32 -0700

    updated the files

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  console.log('Not a number');
-} else {
-  console.log('My number:', parseInt(process.argv[2]));
-}
+const number = Math.floor(Number(process.argv[2]));
+console.log(isNaN(number) ? 'Not a number' : `My number: ${number}`);
